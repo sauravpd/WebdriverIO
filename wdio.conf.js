cc84e684812cd636caf6a9cca4ed6401eb107c0e
Saurav Dhamapurkar <31034114+sauravpd@users.noreply.github.com>
2019-04-29 08:13:10 +0000

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,11 +1,9 @@
 exports.config = {
     // Override the default path of /wd/hub
     path: '/',
-    //
     // ====================
     // Runner Configuration
     // ====================
-    //
     // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
     // on a remote machine).
     runner: 'local'
